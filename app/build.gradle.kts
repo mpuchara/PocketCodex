4e9a41ac190f79dbfdbd552617bccd2680b89443
mpuchara <mpuchara@gmail.com>
2026-09-07 23:56:36 +0200

    Bump PocketCodex to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "pl.homemind.pocketcodex"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     buildTypes {
